263d75045e037ac2648acf5f8a3c13231c9dc0ef
thomaslule <thomas@lule.fr>
2018-08-04 16:50:52 +0200

    use chat history on launch, dont clear on re-launch

--- greet.js
+++ greet.js
@@ -1,26 +1,28 @@
 if (!window.greet_newcomers) {
-  window.greet_newcomers = new Set();
+  window.greet_newcomers = true;
+  const knownViewers = new Set();
   
   const handleNewMessage = messageNode => {
     const viewerNameNode = messageNode.querySelector('.chat-author__display-name');
     const viewerName = viewerNameNode.textContent;
-    if (!window.greet_newcomers.has(viewerName)) {
+    if (!knownViewers.has(viewerName)) {
       viewerNameNode.textContent = '👋 ' + viewerName;
-      window.greet_newcomers.add(viewerName);
+      knownViewers.add(viewerName);
     }
   };
   
-  const handleMutations = mutationRecords => {
+  const chat = document.querySelector('[role=log]');
+
+  chat.querySelectorAll('.chat-line__message').forEach(handleNewMessage);
+  
+  const mo = new MutationObserver(mutationRecords => {
     mutationRecords.forEach(mr => {
       mr.addedNodes.forEach(handleNewMessage);
     });
-  };
-  
-  const chat = document.querySelector('[role=log]');
-  
-  const mo = new MutationObserver(handleMutations);
-  mo.observe(chat, { childList: true, attributes: false, characterData: false, subtree: false });  
+  });
+  mo.observe(chat, { childList: true, attributes: false, characterData: false, subtree: false });
+
+  window.alert('Now showing new viewers! Refresh the page to stop');
 } else {
-  window.greet_newcomers.clear();
+  window.alert('Already showing new viewers. Refresh the page to stop');
 }
-window.alert('Showing new viewers from now on! Refresh the page to stop');
