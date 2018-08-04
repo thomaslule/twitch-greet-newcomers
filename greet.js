9b788c7ebf2a31b5ac8dfb2782aaa504d3979d24
Thomas Lulé <thomas@lule.fr>
2018-08-04 16:17:29 +0200

    Update greet.js

--- greet.js
+++ greet.js
@@ -1,29 +1,26 @@
-if (window.greet_newcomers) {
-  console.log('Already showing new viewers');
-  return;
+if (!window.greet_newcomers) {
+  window.greet_newcomers = new Set();
+  
+  const handleNewMessage = messageNode => {
+    const viewerNameNode = messageNode.querySelector('.chat-author__display-name');
+    const viewerName = viewerNameNode.textContent;
+    if (!window.greet_newcomers.has(viewerName)) {
+      viewerNameNode.textContent = '👋 ' + viewerName;
+      window.greet_newcomers.add(viewerName);
+    }
+  };
+  
+  const handleMutations = mutationRecords => {
+    mutationRecords.forEach(mr => {
+      mr.addedNodes.forEach(handleNewMessage);
+    });
+  };
+  
+  const chat = document.querySelector('[role=log]');
+  
+  const mo = new MutationObserver(handleMutations);
+  mo.observe(chat, { childList: true, attributes: false, characterData: false, subtree: false });  
+} else {
+  window.greet_newcomers.clear();
 }
-
-const knownViewers = new Set();
-
-const handleNewMessage = messageNode => {
-  const viewerNameNode = messageNode.querySelector('.chat-author__display-name');
-  const viewerName = viewerNameNode.textContent;
-  if (!knownViewers.has(viewerName)) {
-    viewerNameNode.textContent = '👋 ' + viewerName;
-    knownViewers.add(viewerName);
-  }
-};
-
-const handleMutations = mutationRecords => {
-  mutationRecords.forEach(mr => {
-    mr.addedNodes.forEach(handleNewMessage);
-  });
-};
-
-const chat = document.querySelector('[role=log]');
-
-const mo = new MutationObserver(handleMutations);
-mo.observe(chat, { childList: true, attributes: false, characterData: false, subtree: false });
-
-console.log('Now showing new viewers');
-window.greet_newcomers = true;
+window.alert('Showing new viewers from now on! Refresh the page to stop');
