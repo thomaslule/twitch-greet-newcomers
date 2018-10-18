8156d9e30021d292fef13aa97d935b70f120f69b
thomaslule <thomas@lule.fr>
2018-10-18 14:32:56 +0200

    display messages in chat instead of alerts

--- greet.js
+++ greet.js
@@ -1,8 +1,17 @@
+const chat = document.querySelector('[role=log]');
+
+function addMessage(message) {
+  const div = chat.appendChild(document.createElement("div"));
+  div.setAttribute("class", "chat-line__status");
+  const span = div.appendChild(document.createElement("span"));
+  span.textContent = message;
+} 
+
 if (!window.greet_newcomers) {
   window.greet_newcomers = true;
   const knownViewers = new Set();
   
-  const handleNewMessage = messageNode => {
+  function handleNewMessage(messageNode) {
     const viewerNameNode = messageNode.querySelector('.chat-author__display-name');
     const viewerName = viewerNameNode.textContent;
     if (!knownViewers.has(viewerName)) {
@@ -11,7 +20,6 @@ if (!window.greet_newcomers) {
     }
   };
   
-  const chat = document.querySelector('[role=log]');
 
   chat.querySelectorAll('.chat-line__message').forEach(handleNewMessage);
   
@@ -22,7 +30,7 @@ if (!window.greet_newcomers) {
   });
   mo.observe(chat, { childList: true, attributes: false, characterData: false, subtree: false });
 
-  window.alert('Now showing new viewers! Refresh the page to stop');
+  addMessage('Now showing new viewers! Refresh the page to stop');
 } else {
-  window.alert('Already showing new viewers. Refresh the page to stop');
+  addMessage('Already showing new viewers. Refresh the page to stop');
 }
