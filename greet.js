59a57255cdaab47275c8d35bc0e47bd29801c47b
thomaslule <thomas@lule.fr>
2018-10-18 15:37:01 +0200

    if already active, reset and restart

--- greet.js
+++ greet.js
@@ -1,36 +1,36 @@
-const chat = document.querySelector('[role=log]');
+const chat = window.document.querySelector('[role=log]');
 
-function addMessage(message) {
-  const div = chat.appendChild(document.createElement("div"));
-  div.setAttribute("class", "chat-line__status");
-  const span = div.appendChild(document.createElement("span"));
-  span.textContent = message;
-} 
-
-if (!window.greet_newcomers) {
-  window.greet_newcomers = true;
-  const knownViewers = new Set();
-  
-  function handleNewMessage(messageNode) {
-    const viewerNameNode = messageNode.querySelector('.chat-author__display-name');
-    const viewerName = viewerNameNode.textContent;
-    if (!knownViewers.has(viewerName)) {
-      viewerNameNode.textContent = '👋 ' + viewerName;
-      knownViewers.add(viewerName);
-    }
-  };
-  
-
-  chat.querySelectorAll('.chat-line__message').forEach(handleNewMessage);
-  
-  const mo = new MutationObserver(mutationRecords => {
-    mutationRecords.forEach(mr => {
-      mr.addedNodes.forEach(handleNewMessage);
-    });
+// if greet_newcomers was already launched, disconnect it and remove the eventual emojis from the chat
+if (window.greet_newcomers) {
+  window.greet_newcomers.disconnect();
+  chat.querySelectorAll('.chat-line__message .chat-author__display-name').forEach((nameNode) => {
+    nameNode.textContent = nameNode.textContent.replace(/^👋 /, '');
   });
-  mo.observe(chat, { childList: true, attributes: false, characterData: false, subtree: false });
-
-  addMessage('Now showing new viewers! Refresh the page to stop');
-} else {
-  addMessage('Already showing new viewers. Refresh the page to stop');
 }
+
+const knownViewers = new Set();
+
+// process a message: if we don't know the viewer, add it to our list and add the emoji
+function handleNewMessage(messageNode) {
+  const viewerNameNode = messageNode.querySelector('.chat-author__display-name');
+  const viewerName = viewerNameNode.textContent;
+  if (!knownViewers.has(viewerName)) {
+    viewerNameNode.textContent = '👋 ' + viewerName;
+    knownViewers.add(viewerName);
+  }
+};
+
+// process all currently displayed messages
+chat.querySelectorAll('.chat-line__message').forEach(handleNewMessage);
+
+// observe the chat, process every new message
+window.greet_newcomers = new MutationObserver(mutationRecords => {
+  mutationRecords.forEach(mr => { mr.addedNodes.forEach(handleNewMessage); });
+});
+window.greet_newcomers.observe(chat, { childList: true, attributes: false, characterData: false, subtree: false });
+
+// display status message in chat
+const div = chat.appendChild(window.document.createElement("div"));
+div.setAttribute("class", "chat-line__status");
+const span = div.appendChild(window.document.createElement("span"));
+span.textContent = 'Now showing new viewers! Refresh the page to stop.';
